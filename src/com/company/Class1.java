a5fefa806c25e05032586077e0e6815a9edabdb7
abvgd10 <abvgda410>
2022-02-09 12:56:47 +0300

    Initial commit

---
+++ src/com/company/Class1.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Class1 {
+        private int nomer;
+        private String soz;
+        private int[] masiv;
+
+        public Class1(int nomer, String soz, int[] masiv) {
+            this.nomer = nomer;
+            this.soz = soz;
+            this.masiv = masiv;
+        }
+
+        private Class1() {
+        }
+
+        public int getNomer() {
+            return nomer;
+        }
+
+        public void setNomer(int nomer) {
+            this.nomer = nomer;
+        }
+
+        public String getSoz() {
+            return soz;
+        }
+
+        public void setSoz(String soz) {
+            this.soz = soz;
+        }
+
+        public int[] getMasiv() {
+            return masiv;
+        }
+
+        public void setMasiv(int[] masiv) {
+            this.masiv = masiv;
+        }
+
+        @Override
+        public String toString() {
+            return "Class{" +
+                    "nomer=" + nomer +
+                    ", soz='" + soz + '\'' +
+                    ", masiv=" + Arrays.toString(masiv) +
+                    '}';
+        }
+    }
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 4, 6, 7, 95, 34};
+
+        Class1 class1 = new Class1(5, "Беш", a);
+        System.out.println(class1);
+
+    }
+}
